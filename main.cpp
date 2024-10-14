b2ac87aac9501d00bf887ec7044293f9f6f3b902
Dhanraj Jadhav <dj004786@gmail.com>
2024-10-14 09:35:30 +0530

    making main file and writting basic code

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include"encryption.h"
+using namespace std;
+
+int main() {
+
+    string filename;
+    char mode;
+
+    cout << "Enter the file name: ";
+    getline(cin >> ws, filename);   // to get file name which can include wide spaces
+
+    cout << "Encrypt (e) or Decrypt (d)?";
+    cin >> mode;
+
+    if(mode == 'e' || mode == 'E') {
+        
+    } 
+    return 0;
+}
